8f3bd3fb617911650066d3c1a59d8602e38baaa6
Zintis ICS3U <zintis.kalins@student.tdsb.on.ca>
2024-04-11 13:24:37 -0400

    completed assignment

--- java-variables/src/JavaVariables.java
+++ java-variables/src/JavaVariables.java
@@ -10,9 +10,13 @@ public class JavaVariables {
                 System.out.println("My monthly allowance = " + allowance);
                 System.out.println("Cost of a single battle pass = " + battlepass);
         //With the current allowance, how many battle passes can I buy
-        //divide allowance by cost of a battlepass
-                double battlepassNum = allowance / battlepass;
-        //print the number of battlepasses i can buy
-                System.out.println("I can buy " + (int) battlepassNum + " battlepasses with an allowance of " + allowance + " dollars.");
+                //divide allowance by cost of a battlepass
+                double NumBattlepass = allowance / battlepass;
+                //print the number of battlepasses i can buy
+                System.out.println("I can buy " + (int) NumBattlepass + " battlepasses with an allowance of $" + allowance);
+        //what is the total cost of battlepasses
+                //cost is equal to the number of battlepasses that can be bought times the cost of a battlepass
+                double costTotal = (int) NumBattlepass * battlepass;
+                System.out.println("To buy " + (int) NumBattlepass + " battlepasses, it will cost me $" + costTotal);
                 }
 }
